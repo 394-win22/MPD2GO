5ab6fc87a10195fc95f334c509888e1ace51ae47
Stanley Jia <stanleyjia101@gmail.com>
2022-02-14 17:38:56 -0600

    ux components

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {useUserState, signInWithGoogle} from "./utilities/firebase.js"; 
+import {useUserState, signInWithGoogle} from "./utilities/firebase.js";
+import CreatePost from './components/CreatePost.js'
 
 function App() {
 
   const [user] = useUserState();
-  
+
   return (
     <>
       (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+					<Route path="/createPost" element={<CreatePost />} />
         </Routes>
         {/* {user && <BottomMenu user={user} />} */}
       </BrowserRouter>)
