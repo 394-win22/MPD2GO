c34a12e5299aef9ca93b3ba4f36f0323438c904a
Sammi Huang <96802164+SammiH2022@users.noreply.github.com>
2022-02-14 17:58:30 -0600

    styling createPost

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,4 +1,4 @@
-import { Box, Typography, TextField, Button} from "@mui/material";
+import { Stack, Box, Typography, TextField, Button} from "@mui/material";
 import { makeStyles } from "@mui/styles";
 
 
@@ -17,7 +17,7 @@ const CreatePost = ({}) => {
   return (
     <Box >
 			<Typography>Create Post</Typography>
-			<Box className={classes.container}>
+			<Box sx={{'& .MuiTextField-root': { m: 1, width: '50ch' },}} className={classes.container}>
 				<TextField id="outlined-basic"
 				label="Title"
 				variant="outlined" />
@@ -25,13 +25,14 @@ const CreatePost = ({}) => {
 						id="outlined-multiline-flexible"
 						label="Text (optional)"
 						multiline
-						maxRows={4}
+						minRows={4}
 						// value={value}
 						// onChange={handleChange}
 					/>
-
-					<Button variant="contained">Cancel</Button>
-					<Button variant="contained">Post</Button>
+					<Stack spacing={2} direction="row">
+						<Button variant="contained">Cancel</Button>
+						<Button variant="contained">Post</Button>
+					</Stack>
 				</Box>
 
 
