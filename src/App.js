4f13bc4a7386be58481b524936041f10929ce82c
Pengxiang Zhang <z-git@protonmail.com>
2022-02-14 17:45:49 -0600

    minor bug fix

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useUserState } from "./utilities/firebase.js"; 
 import Welcome from './components/Welcome'
+import CreatePost from './components/CreatePost'
 
 function App() {
   const [user] = useUserState();
