2c5f6c78b65811df165cd82d935a050f1d62264f
Fortune Emmanuel-king <fortuneemmanuel-king2023@u.northwestern.edu>
2022-02-14 18:30:00 -0600

    handleSubmit with bugs

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,5 +1,6 @@
 import { Stack, Box, Typography, TextField, Button} from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import {useState} from "react";
 
 
 const useStyles = makeStyles({
@@ -12,28 +13,40 @@ const useStyles = makeStyles({
 
 });
 
+
 const CreatePost = ({}) => {
+	
+	const [title, setTitle] = useState("");
+	const [description, setDescription] = useState("");
+
 	const classes = useStyles();
+	const handleSubmit = async (e)=>{
+		console.log(title , description)
+		//createPostInFirebase();
+   	};
   return (
+	
     <Box >
 			<Typography>Create Post</Typography>
 			<Box sx={{'& .MuiTextField-root': { m: 1, width: '50ch' },}} className={classes.container}>
 				<TextField id="outlined-basic"
 				label="Title"
-				variant="outlined" />
+				value = {title}
+				variant="outlined"
+				onChange ={(value)=> {setTitle(value)}} />
 				<TextField
 						id="outlined-multiline-flexible"
 						label="Text (optional)"
 						multiline
 						minRows={4}
-						// value={value}
-						// onChange={handleChange}
+						value={description}
+						onChange= {(value)=> {setDescription(value)}}
 					/>
 					<Stack spacing={2} direction="row">
 						<Button variant="contained">Cancel</Button>
-						<Button variant="contained">Post</Button>
+						<Button variant="contained" type="submit" onClick={handleSubmit} >Post</Button>
 					</Stack>
-				</Box>
+			</Box>
 
 
     </Box>
