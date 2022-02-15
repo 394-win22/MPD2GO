fffa0f5d1fa88074d94293337e2cbb43c8214222
Hao Zhang <haozhangzyb@outlook.com>
2022-02-14 18:34:25 -0600

    Hao: add comment box

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,7 @@ import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
-import { Button } from "@mui/material";
+import { Button, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import Collapse from "@mui/material/Collapse";
 import Avatar from "@mui/material/Avatar";
@@ -13,6 +13,14 @@ import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
 import Comment from "./Comment.js";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import SendIcon from '@mui/icons-material/Send';
+
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -54,11 +62,33 @@ export default function Post() {
 
       <CardContent align="left" style={{ backgroundColor: "#eceff1" }}>
         <Button> View more comments </Button>
-        <Box>
-          <Comment></Comment>
-        </Box>
+        <Box >
+          <Comment />
+        </Box>          
       </CardContent>
-      {/* </Collapse> */}
+      <Paper
+      component="form"
+      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: "100%" }}
+      >
+        <IconButton sx={{ p: '10px' }} aria-label="menu">
+          <Avatar aria-label="recipe">
+            ICON
+          </Avatar>
+        </IconButton>
+        
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Add comments here"
+          inputProps={{ 'aria-label': 'Add comments here' }}
+        />
+        
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        
+        <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+          <SendIcon />
+        </IconButton>
+      </Paper>
+     {/* </Collapse> */}
     </Card>
   );
 }
