8d3ba136e64021892e366bc84433da7c170b2e73
Stanley Jia <stanleyjia101@gmail.com>
2022-02-17 00:50:33 -0600

    Revert "reverting post to my branch"

--- src/components/Post.js
+++ src/components/Post.js
@@ -13,9 +13,30 @@ import { Typography, IconButton } from "@mui/material/";
 
 import { makeStyles } from "@mui/styles";
 import { useNavigate } from "react-router-dom";
-const getUserFromUID = (uid, users) => {
-  return users.filter((user) => user.uid === uid)[0];
-};
+import { red } from "@mui/material/colors";
+import Comment from "./Comment.js";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import SendIcon from '@mui/icons-material/Send';
+
+import Thread from "./Thread"
+import exampleData from "../exampleData.json"
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 const useStyles = makeStyles({
   card: {
@@ -33,7 +54,13 @@ const useStyles = makeStyles({
 
 export default function Post({ posts, users, post }) {
   const classes = useStyles();
-  const user = getUserFromUID(post.author, users);
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  
   const navigate = useNavigate();
 
   return (
@@ -48,6 +75,39 @@ export default function Post({ posts, users, post }) {
           {post.description}
         </Typography>
       </CardContent>
+        <CardContent align="left" style={{ backgroundColor: "#eceff1" }}>
+
+					<Thread data={exampleData.posts["randomPostId"].threads["commentId"]}/>
+
+
+        </CardContent>
+
+      {/* Comment box start here */}
+      <Paper
+        component="form"
+        sx={{
+          p: "2px 4px",
+          display: "flex",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <IconButton sx={{ p: "10px" }} aria-label="menu">
+          <Avatar aria-label="recipe">ICON</Avatar>
+        </IconButton>
+
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Add comments here"
+          inputProps={{ "aria-label": "Add comments here" }}
+        />
+
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+
+        <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+          <SendIcon />
+        </IconButton>
+      </Paper>
     </Card>
   );
 }
