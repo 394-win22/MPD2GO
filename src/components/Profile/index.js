8b057558b02479916801ec4b40dd790b3918dfae
cxduvall <57238295+cxduvall@users.noreply.github.com>
2022-02-23 15:05:54 -0600

    basic profile layout

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import { Typography, Avatar, Box } from '@mui/material'
+import { Typography, Avatar, Box, Divider, Button, Stack } from '@mui/material'
 import { EditUserButton } from '../EditProfile/EditUserButton'
 import { getUserFromUid } from 'utilities/firebase'
+import { AccountCircle as AccountCircleIcon, Email as EmailIcon, Home as HomeIcon, MoreHoriz as MoreHorizIcon, AddCircle as AddCircleIcon } from '@mui/icons-material'
 
 const Profile = ({ user }) => {
   const params = useParams()
@@ -38,20 +39,28 @@ const Profile = ({ user }) => {
         }}
       />
       <Typography
-        variant='h3'
+        variant='h4'
         component='div'
-        sx={{ flexGrow: 1, paddingLeft: 1, paddingBottom: 5, marginBottom: "15px" }}
+        sx={{ flexGrow: 1, paddingLeft: 1, paddingBottom: 1, marginBottom: "0px" }}
       >
         {userData.displayName}
       </Typography>
       <Typography
         variant='h8'
         component='div'
+        sx={{ flexGrow: 1, paddingLeft: 1, paddingBottom: 5, marginBottom: "5px" }}
+      >
+        Chicago, IL [PLACE]
+      </Typography>
+      <Divider/>
+      <Typography
+        variant='h8'
+        component='div'
         sx={{ flexGrow: 1, paddingLeft: 1, paddingTop: 5 }}
       >
-        {userData.year ? "Class of " + userData.year : 'No Year'}
+        {userData.year ? "MPD2 Class of " + userData.year : 'No Year'}
       </Typography>
-      <Typography
+      {/*<Typography
         variant='h6'
         component='div'
         sx={{ flexGrow: 1, paddingLeft: 1, paddingTop: 5 }}
@@ -67,7 +76,49 @@ const Profile = ({ user }) => {
         userID={user.uid}
       />
       )}
+      </Typography>*/}
+      <Stack direction="row" align="center">
+        <Typography align="center">
+          Current student
+        </Typography>
+        <Button align="center">
+          Green team
+        </Button>
+      </Stack>
+      <Button variant="contained"
+          color="success"
+          sx={{marginBottom: 3}}>
+        View Capstone Page
+      </Button>
+      <Divider/>
+      <Typography align="left" sx={{marginBottom: 3}}>
+          Expertise
       </Typography>
+      <Stack direction="row"
+        sx={{marginBottom: 3}}>
+        {["Rocket Science", "Product Design"].map(x => (
+         <Button style={{
+            borderRadius: 15,
+            backgroundColor: "#B6B6B6",
+            padding: "3px 6px",
+            fontSize: "10px"
+        }}
+        variant="contained">
+          {x}
+        </Button>
+        ))}
+      </Stack>
+      <Divider/>
+      <Stack direction="row"
+        sx={{marginBottom: 3}}>
+          <EmailIcon/>
+          <Typography>mail link</Typography>
+      </Stack>
+      <Stack direction="row"
+        sx={{marginBottom: 3}}>
+        <AddCircleIcon/>
+        <Typography>linkedIn link</Typography>
+      </Stack>
     </Box>
   )
 }
