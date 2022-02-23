ecc8184daa94903a6587fbbc857f022c79f63195
amyeguo <72362221+amyeguo@users.noreply.github.com>
2022-02-23 17:27:05 -0600

    begin editProject

---
+++ src/components/EditProject/EditProjectButton.js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import Button from "@mui/material/Button";
+
+export const EditProjectButton = ({ project, projectId }) => {
+  return (
+    <>
+      <Button variant="contained">Edit Project</Button>
+    </>
+  );
+};
