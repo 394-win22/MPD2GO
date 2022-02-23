cf68f4839974b67d976b8a4a9bf91b9e587a9bfc
Pengxiang Zhang <z-git@protonmail.com>
2022-02-23 16:42:53 -0600

    Minor bug fix profile page error

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,15 @@
 import { useEffect, useState } from "react";
-import { useParams,useNavigate } from "react-router";
-import { Typography, Avatar, Box,Paper, Divider, Button, Stack, Card } from "@mui/material";
+import { useParams, useNavigate } from "react-router";
+import {
+  Typography,
+  Avatar,
+  Box,
+  Paper,
+  Divider,
+  Button,
+  Stack,
+  Card,
+} from "@mui/material";
 import { EditUserButton } from "../EditProfile/EditUserButton";
 import { getUserFromUid } from "utilities/firebase";
 import {
@@ -33,7 +42,7 @@ const Profile = ({ user }) => {
               setProjectData(dataProject);
             }
           });
-        }
+        } else setProjectData("not found");
       }
     });
   }, [params, user]);
@@ -46,128 +55,129 @@ const Profile = ({ user }) => {
 
   return (
     <>
-    <Button
-      sx={{ ml: 1, mb: 2, color: "white" }}
-      variant="contained"
-      onClick={() => {
-        navigate(-1);
-      }}
-    >
-      Back
-    </Button>
-    <Card sx={{ px: 4, py:4, mb: 10 }} style={{ borderRadius: 10 }}>
-    <Box textAlign="center">
-      <Avatar
-        alt={userData.displayName}
-        src={userData.photoURL}
-        variant="rounded"
-        sx={{
-          height: 1 / 6,
-          width: 1 / 6,
-          margin: "auto",
-          borderRadius: "50%",
-          my: 1,
-        }}
-      />
-      <Typography
-        variant="h4"
-        component="div"
-        sx={{
-          flexGrow: 1,
-          paddingLeft: 1,
-          paddingBottom: 1,
-          marginBottom: "0px",
+      <Button
+        sx={{ ml: 1, mb: 2, color: "white" }}
+        variant="contained"
+        onClick={() => {
+          navigate(-1);
         }}
       >
-        {userData.displayName}
-      </Typography>
-      <Typography
-        variant="body1"
-        display="block"
-        sx={{ my: 1 }}
-        style={{ color: "#7B7B7B" }}
-      >
-        {userData.location ? userData.location : "Unknown Location"}
-      </Typography>
-      <Divider />
-      <Typography
-        variant="body1"
-        display="block"
-        style={{ color: "#7B7B7B" }}
-        sx={{ flexGrow: 1, paddingLeft: 1, my: 1 }}
-      >
-        {userData.year ? "Class of " + userData.year : "No Year"}
-      </Typography>
-      <Typography
-        variant="body1"
-        display="block"
-        style={{ color: "#7B7B7B" }}
-        sx={{ flexGrow: 1, paddingLeft: 1, my: 1 }}
-      >
-        {userData.status ? userData.status : "Unknown Status"}
-        {"teamId" in userData && (
-          <Chip
-            size="small"
-            label={projectData.name}
-            variant="outlined"
-            sx={{ mx: 1 }}
-          />
-        )}
-      </Typography>
-      {"teamId" in userData && (
-        <Button
-          variant="contained"
-          onClick={() => {
-            navigate(`/project/${userData.teamId}`);
-          }}
-          sx={{ m: 1 }}
-        >
-          <InsertDriveFileIcon />
-          View Capstone Page
-        </Button>
-      )}
-      <Typography
-        variant="h6"
-        component="div"
-        sx={{ flexGrow: 1, paddingLeft: 1, paddingTop: 5 }}
-      >
-        {userData.bio ? userData.bio : "No Bio"}
-
-        <Divider />
-      </Typography>
-      <Typography align="left" sx={{ marginBottom: 3 }}>
-        Expertise
-      </Typography>
-      <Stack direction="row" sx={{ marginBottom: 3 }}>
-        {["Rocket Science", "Product Design"].map((x) => (
-          <Button
-            style={{
-              borderRadius: 15,
-              backgroundColor: "#B6B6B6",
-              padding: "3px 6px",
-              fontSize: "10px",
+        Back
+      </Button>
+      <Card sx={{ px: 4, py: 4, mb: 10 }} style={{ borderRadius: 10 }}>
+        <Box textAlign="center">
+          <Avatar
+            alt={userData.displayName}
+            src={userData.photoURL}
+            variant="rounded"
+            sx={{
+              height: 1 / 6,
+              width: 1 / 6,
+              margin: "auto",
+              borderRadius: "50%",
+              my: 1,
             }}
-            variant="contained"
+          />
+          <Typography
+            variant="h4"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              paddingLeft: 1,
+              paddingBottom: 1,
+              marginBottom: "0px",
+            }}
           >
-            {x}
-          </Button>
-        ))}
-      </Stack>
-      <Divider />
-      <Stack direction="row" sx={{ marginBottom: 3 }}>
-        <EmailIcon />
-        <Typography>mail link</Typography>
-      </Stack>
-      <Stack direction="row" sx={{ marginBottom: 3 }}>
-        <AddCircleIcon />
-        <Typography>linkedIn link</Typography>
-      </Stack>
-      {(!params.userID || params.userID === user.uid) && (
-        <EditUserButton key={userData} user={userData} userID={user.uid} />
-      )}
-    </Box>
-    </Card>
-  </>);
+            {userData.displayName}
+          </Typography>
+          <Typography
+            variant="body1"
+            display="block"
+            sx={{ my: 1 }}
+            style={{ color: "#7B7B7B" }}
+          >
+            {userData.location ? userData.location : "Unknown Location"}
+          </Typography>
+          <Divider />
+          <Typography
+            variant="body1"
+            display="block"
+            style={{ color: "#7B7B7B" }}
+            sx={{ flexGrow: 1, paddingLeft: 1, my: 1 }}
+          >
+            {userData.year ? "Class of " + userData.year : "No Year"}
+          </Typography>
+          <Typography
+            variant="body1"
+            display="block"
+            style={{ color: "#7B7B7B" }}
+            sx={{ flexGrow: 1, paddingLeft: 1, my: 1 }}
+          >
+            {userData.status ? userData.status : "Unknown Status"}
+            {"teamId" in userData && (
+              <Chip
+                size="small"
+                label={projectData.name}
+                variant="outlined"
+                sx={{ mx: 1 }}
+              />
+            )}
+          </Typography>
+          {"teamId" in userData && (
+            <Button
+              variant="contained"
+              onClick={() => {
+                navigate(`/project/${userData.teamId}`);
+              }}
+              sx={{ m: 1 }}
+            >
+              <InsertDriveFileIcon />
+              View Capstone Page
+            </Button>
+          )}
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, paddingLeft: 1, paddingTop: 5 }}
+          >
+            {userData.bio ? userData.bio : "No Bio"}
+
+            <Divider />
+          </Typography>
+          <Typography align="left" sx={{ marginBottom: 3 }}>
+            Expertise
+          </Typography>
+          <Stack direction="row" sx={{ marginBottom: 3 }}>
+            {["Rocket Science", "Product Design"].map((x) => (
+              <Button
+                style={{
+                  borderRadius: 15,
+                  backgroundColor: "#B6B6B6",
+                  padding: "3px 6px",
+                  fontSize: "10px",
+                }}
+                variant="contained"
+              >
+                {x}
+              </Button>
+            ))}
+          </Stack>
+          <Divider />
+          <Stack direction="row" sx={{ marginBottom: 3 }}>
+            <EmailIcon />
+            <Typography>mail link</Typography>
+          </Stack>
+          <Stack direction="row" sx={{ marginBottom: 3 }}>
+            <AddCircleIcon />
+            <Typography>linkedIn link</Typography>
+          </Stack>
+          {(!params.userID || params.userID === user.uid) && (
+            <EditUserButton key={userData} user={userData} userID={user.uid} />
+          )}
+        </Box>
+      </Card>
+    </>
+  );
 };
 
 export default Profile;
