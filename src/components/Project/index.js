3b4f1a37f419ccfa78995cb94239e78af22e34de
collinmurch <collin@murch.net>
2022-02-23 18:27:24 -0600

    Working Resources

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -10,14 +10,15 @@ import {
   Box,
   Button
 } from "@mui/material";
+import LinkIcon from '@mui/icons-material/Link'
 import moment from "moment";
 import Chip from "@mui/material/Chip";
 import ReactGoogleSlides from "react-google-slides";
 import { getProjectFromUid } from "../../utilities/firebase";
 import { getUserDataFromUID } from "../../utilities/posts"
 import { UserContext } from "components/LoggedIn";
-import driveLogo from 'google-drive.png'
-import muralLogo from './../../mural.png'
+import DriveLogo from 'google-drive.png'
+import MuralLogo from 'mural.png'
 
 const Project = () => {
   const navigate = useNavigate()
@@ -38,6 +39,7 @@ const Project = () => {
   if (!projectData) {
     return <h1 style={{ marginLeft: 20 }}>Loading...</h1>;
   }
+
   return (
     <>
       <Button
@@ -114,40 +116,23 @@ const Project = () => {
             showControls
             loop
           />
-          { (projectData.drive || projectData.mural) && 
+          { projectData.resources !== undefined && Object.values(projectData.resources).length > 0 && 
           <>
             <Typography variant="h6" align="left" sx={{ my: 1 }}>
               Additional Resources
             </Typography>
-            {projectData.drive &&
-            <>
-              <Button sx={{ marginLeft: '8px' }}
-                startIcon={<CardMedia
-                  component='img'
-                  style={{width: 20, height: 20}}
-                  image={driveLogo}
-                  alt='drive'
-                />}
-                onClick={() => {
-                  window.open(projectData.drive)
-                }}
-              >Project Folder</Button>
-              </>}
-            {projectData.mural &&
+            {Object.values(projectData.resources).map((resource) => (
               <>
-                <Button sx={{ marginLeft: '8px' }}
-                  startIcon={<CardMedia
-                    component='img'
-                    style={{width: 20, height: 20}}
-                    image={muralLogo}
-                    alt='mural'
-                  />}
-                  onClick={() => {
-                    window.open(projectData.mural)
-                  }}
-                >Project Mural</Button>
-              </>}
-            </>}
+              <Button sx={{ marginLeft: '8px' }}
+                startIcon={(resource.url.includes('mural')) ? <img src={MuralLogo} style={{height: 20, width: 20}}/> : (
+                  (resource.url.includes('drive') ? <img src={DriveLogo} style={{height: '20px', width: '20px'}}/> : <LinkIcon />))}
+                onClick={() => {
+                  window.open(resource.url)
+                }}
+              >{resource.text}</Button>
+              </>
+            ))}
+          </>}
         </CardContent>
       </Card>
     </>
