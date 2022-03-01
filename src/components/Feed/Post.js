e9fc75ca1651201a11ad9f3c7ce3a49508e479f9
collinmurch <collin@murch.net>
2022-02-28 18:30:53 -0600

    Small styling fix

--- src/components/Feed/Post.js
+++ src/components/Feed/Post.js
@@ -53,10 +53,10 @@ const Post = ({ post }) => {
       <AvatarWithTag user={user} post={post} />
 
       <CardContent sx={{ pt: 0, px: 0 }}>
-        <RichTextEditor readOnly value={post.description} style={{border: 'none'}}/>
+        <RichTextEditor readOnly value={post.description} style={{border: 'none', marginBottom: -15}}/>
 
 
-        <Stack direction="row" spacing={1} sx={{mt:2,overflowX: "scroll"}}>
+        <Stack direction="row" spacing={1} sx={{mt:2,overflowX: "scroll", paddingLeft: 2}}>
 
         {"tags" in post &&
           post.tags.map((tag, i) => (
