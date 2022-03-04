d1e5e7387b52f49c9656a9e8a80486ba961f0d91
Stanley Jia <stanleyjia101@gmail.com>
2022-03-04 15:33:39 -0600

    styling

--- src/components/AvatarWithTag/AvatarWithTag.js
+++ src/components/AvatarWithTag/AvatarWithTag.js
@@ -14,6 +14,7 @@ const AvatarWithTag = ({ user, post }) => {
     teamData = projectList.find((obj) => obj.uid === user.teamId);
   }
 
+
   return (
     <CardHeader
       align="left"
@@ -28,20 +29,26 @@ const AvatarWithTag = ({ user, post }) => {
         />
       }
       title={
-        <Stack direction="row">
+        <Stack direction="row" spacing={3}>
           <Typography>{user.displayName}</Typography>
           {"teamId" in user && (
             <Chip
-              icon={
-                <InsertDriveFileIcon style={{ color: teamData.textColor }} />
-              }
               size="small"
-              label="Capstone Page"
+              label={teamData.name}
               variant="outlined"
               sx={{
+                marginLeft: "30px !important",
                 mx: 1,
                 backgroundColor: teamData.teamColor,
                 color: teamData.textColor,
+                border: 0,
+                "&:hover": {
+                  backgroundColor: shade(teamData.teamColor, -0.3) + " !important"
+                },
+                "&:active": {
+                  backgroundColor: shade(teamData.teamColor, -0.5) + " !important"
+                },
+
               }}
               onClick={(event) => {
                 event.stopPropagation();
@@ -56,4 +63,39 @@ const AvatarWithTag = ({ user, post }) => {
   );
 };
 
+function hex2(c) {
+  c = Math.round(c);
+  if (c < 0) c = 0;
+  if (c > 255) c = 255;
+
+  var s = c.toString(16);
+  if (s.length < 2) s = "0" + s;
+
+  return s;
+}
+
+function color(r, g, b) {
+  return "#" + hex2(r) + hex2(g) + hex2(b);
+}
+
+function shade(col, light) {
+
+  // TODO: Assert that col is good and that -1 < light < 1
+
+  var r = parseInt(col.substr(1, 2), 16);
+  var g = parseInt(col.substr(3, 2), 16);
+  var b = parseInt(col.substr(5, 2), 16);
+
+  if (light < 0) {
+    r = (1 + light) * r;
+    g = (1 + light) * g;
+    b = (1 + light) * b;
+  } else {
+    r = (1 - light) * r + light * 255;
+    g = (1 - light) * g + light * 255;
+    b = (1 - light) * b + light * 255;
+  }
+
+  return color(r, g, b);
+}
 export default AvatarWithTag;
