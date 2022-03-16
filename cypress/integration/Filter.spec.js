2ed1f8b9991cef406b7f18bc5bd6dfd3fe81205a
4tune_kvng <fortuneemmanuel-king2023@u.northwestern.edu>
2022-03-15 23:58:07 -0500

    filter testing

---
+++ cypress/integration/Filter.spec.js
@@ -0,0 +1,50 @@
+// Filter.spec.js created with Cypress by Fortune Emmanuel-King
+//
+// Cypress.Commands.add("visitDirectory", () => {
+//     cy.visit("/");
+// });
+
+
+describe('Testing Filters in Home page', function(){
+    it("Shows filters", () => {
+        cy.visit("/");
+        cy.wait(1000);
+        cy.get('.MuiInputBase-input.css-yz9k0d-MuiInputBase-input').should("be.visible").click();
+        // cy.contains("ExampleTestTeam").should("be.visible");
+        cy.contains("Finance").should("be.visible");
+        cy.contains("Marketing").should("be.visible");
+        cy.contains("Software Development").should("be.visible");
+    });
+
+    it("Show all posts with Tag", ()=>{
+        cy.visit("/");
+        cy.wait(1000);
+        cy.get('.MuiInputBase-input.css-yz9k0d-MuiInputBase-input').should("be.visible").click();
+        cy.contains("Marketing").should("be.visible").click();
+        cy.contains("Testing User 2").should("be.visible");
+        cy.contains("Testing User 1").should("be.visible");
+    });
+
+    it("Show only posts with multiple clicked tags", ()=>{
+        cy.visit("/");
+        cy.wait(1000);
+        cy.get('.MuiInputBase-input.css-yz9k0d-MuiInputBase-input').should("be.visible").click();
+        cy.contains("Industrial Design").should("be.visible").click();
+        cy.contains("Testing User 2").should("be.visible");
+        cy.contains("Testing User 1").should("not.exist");
+        cy.contains("Software Development").should("be.visible").click();
+        cy.contains("Testing User 1").should("be.visible");
+    });
+
+    it("Team 1 filter", ()=>{
+        cy.visit("/");
+        cy.wait(1000);
+        cy.get('.MuiInputBase-input.css-yz9k0d-MuiInputBase-input').should("be.visible").click();
+        cy.contains("Test Team 2").should("be.visible").click();
+        cy.contains("Testing User 2").should("be.visible");
+        cy.contains("Testing User 1").should("not.exist");
+    });
+
+
+
+})
